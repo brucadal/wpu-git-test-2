cafc830eb4a8c15a6e78992b1e7bd6a353ae5dce
brcadal <brucadal@gmail.com>
2024-10-27 05:29:33 +0700

    manambahkan pargraf lagi

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum natus reprehenderit laborum magni enim ex ipsa exercitationem unde deleniti modi tempora libero facilis, amet laudantium dolorum voluptates rem veritatis accusamus?</p>
+    <p>ksks</p>
 </body>
 </html>
